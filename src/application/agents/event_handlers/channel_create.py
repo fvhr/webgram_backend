5abb2afbf21f1da18727476e8ad68a877736e3ed
Ткаченко Роман Александрович <RomanTkachenko@ttk.ttk.ru>
2026-05-04 08:45:58 +0300

    websocket roles

--- src/application/agents/event_handlers/channel_create.py
+++ src/application/agents/event_handlers/channel_create.py
@@ -17,6 +17,7 @@ class ChannelCreateEventHandler(EventHandler):
     async def __call__(self, event: EventDTO) -> None:
         await asyncio.sleep(0.5)  # в апи появляется позже чем приходит событие
         ws_response = await self._get_calls_service.get_calls()
+        print(ws_response)
         await self._ws_manager.broadcast_message_to_role(WebsocketMessageTypes.UPDATE_CALLS, ws_response,
                                                  WebsocketRoles.ADMIN)
 
